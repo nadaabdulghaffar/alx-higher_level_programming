221c20107e4cce0e09b04da08ec9d8ab3d2d62d6
Nada <nadaabdulghaffar42@gmail.com>
2024-01-19 03:15:43 +0200

    task 3.2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    value = len(sys.argv) - 1
-    if value == 0:
-        print("0 arguments.")
-    elif value == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(value))
-    for i in range(value):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        import sys
+        value = len(sys.argv) - 1
+        if value == 0:
+            print("0 arguments.")
+        elif value == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(value))
+        for i in range(value):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
