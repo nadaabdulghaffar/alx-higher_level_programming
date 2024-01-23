89e50dfb3dc30b94007b6385e123247a5238c5cf
Nada <nadaabdulghaffar42@gmail.com>
2024-01-23 22:13:19 +0200

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     matrix_lists = len(matrix)
     for i in range(matrix_lists):
         for j in range(len(matrix[i])):
