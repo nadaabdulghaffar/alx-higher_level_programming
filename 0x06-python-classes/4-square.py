d2a579fa0b6e4f7f29864a6222eb3bdb6be7ee26
Nada <nadaabdulghaffar42@gmail.com>
2024-02-16 00:38:20 +0200

    task 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Defines Square Class"""
+    def __init__(self, size=0):
+        """Constructor
+        Args:
+            size: length of side of the square (default 0)
+
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+
+    def area(self):
+        """Area of the square
+            Return: The Area of the square"""
+        return self.__size ** 2
+
+
+    def size(self):
+        """Retireve length size
+            Return: size"""
+        return self.__size
+
+
+    def size(self, value):
+        """Set value to length size
+        Args:
+            value: length to side square
+        Raises:
+            TypeError: If size not Integer
+            ValueError: If size is less than zero
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
