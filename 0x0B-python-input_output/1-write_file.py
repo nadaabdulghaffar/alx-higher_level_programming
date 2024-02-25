0c6822c8a2d39e37b5d46b26675b8900517702e3
Nada <nadaabdulghaffar42@gmail.com>
2024-02-25 23:54:19 +0200

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define write_file methode"""
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a text file
+        Return: the number of characters
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
