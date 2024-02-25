fd674903e41de6edf86c98b8f29c4417a42a843e
Nada <nadaabdulghaffar42@gmail.com>
2024-02-25 23:36:17 +0200

    task 0.1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it"""
     with open(filename, encoding="utf-8") as file:
-        content=file.read()
+        content = file.read()
         print(content, end="")
