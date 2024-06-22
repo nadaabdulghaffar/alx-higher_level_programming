af1feaef85307c618d7b15ada9def4790c3a641c
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-22 21:51:28 +0300

    Task 10 & 11

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+function Factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else {
+    return (n * Factorial(n - 1));
+  }
+}
+console.log(Factorial(process.argv[2]));
