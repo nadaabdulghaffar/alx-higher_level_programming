918c2d988745a43bf38465f4f06391d1f6b9a726
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-22 21:25:39 +0300

    Task 7 - 9

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const args = process.argv[2];
+const size = parseInt(args);
+if (args === undefined || isNaN(args)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
