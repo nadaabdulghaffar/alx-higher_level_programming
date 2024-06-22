cdea69511a0db64ee65c2a46c342bf17b6208763
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-22 20:10:24 +0300

    MultiLines:Fixed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const arry = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arry = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < arry.length; i++) {
   console.log(arry[i]);
