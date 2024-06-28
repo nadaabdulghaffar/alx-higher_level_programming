d55beceb6d13dffb6c8e7e9eff0ac564338aae00
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-28 18:40:26 +0300

    Model State

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+file that contains the class definition of a State
+"""
+
+from sqlalchemy import Columan, String, Integer
+from sqlalchemy.esxt.declarative import declarative_base
+
+Base  = declarative_base()
+
+class State(Base):
+    """
+    class of the definition of a State and an instance Base
+    states table:
+        id:Primary Key
+        name: name of the state 
+    """
+
+    __tablename__ = 'states'
+    id = Columan(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Columan(String(128), nullable=False)
