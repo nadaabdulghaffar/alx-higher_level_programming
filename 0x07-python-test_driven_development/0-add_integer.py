a6e0071d186c3e965b37cff7e2796594da034533
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-30 23:03:12 +0300

    task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,9 +9,9 @@ def add_integer(a, b=98):
         return:
             sum of two numbers
         """
-    if type(a) not in (float, int):
+    if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    if type(b) not in (float, int):
+    if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
