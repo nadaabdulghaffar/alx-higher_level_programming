fec070c964d3a96ef7cb2e4a9227448ce1a94276
=Nada <nadaabdulghaffar42@gmail.com>
2024-06-30 23:07:28 +0300

    task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,17 +2,28 @@
 """ Define add_integer Module"""
 
 
+import math
+
 def add_integer(a, b=98):
     """function that adds 2 integers.
         Raise:
             TypeError if a or b not int or float
+            ValueError if a or b is NaN
+            OverflowError if a or b is too large
         return:
-            sum of two numbers
+            sum of two number
         """
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
+
+    if math.isnan(a) or math.isnan(b):
+        raise ValueError()
+
+    if math.isinf(a) or math.isinf(b):
+        raise OverflowError()
+    
     return int(a) + int(b)
 
 
