3980d98244999aa6bcee22026e76753cb0a7e1f4
=Nada <nadaabdulghaffar42@gmail.com>
2024-07-27 02:23:26 +0300

    task 9: Spaces missing

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- creates a table second_table in the database add multiples rows.
-CREATE TABLE 
+CREATE TABLE
 	IF NOT EXISTS second_table (
 		id INT,
 		name VARCHAR(256),
@@ -7,7 +7,8 @@ CREATE TABLE
 		PRIMARY KEY (id)
 	);
 
-INSERT INTO second_table(id, name, score)
+INSERT INTO 
+	second_table (id, name, score)
 VALUES 
 		(1, 'John', 10),
 		(2, 'Alex', 3),
