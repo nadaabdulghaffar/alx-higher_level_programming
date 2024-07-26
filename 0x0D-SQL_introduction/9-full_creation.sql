c90e81b12421de5ff611541d6593eccf6a9a178e
=Nada <nadaabdulghaffar42@gmail.com>
2024-07-27 02:27:07 +0300

    task 9.5

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -10,7 +10,7 @@ CREATE TABLE
 INSERT INTO
 	second_table (id, name, score)
 VALUES
-		(1, 'John', 10),
-		(2, 'Alex', 3),
-		(3, 'Bob', 7),
-		(4, 'George', 8);
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
