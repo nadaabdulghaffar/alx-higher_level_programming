7050be7f8aac12d124c1534302118d9d10efac9b
=Nada <nadaabdulghaffar42@gmail.com>
2024-07-27 02:20:42 +0300

    task 9.2

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,15 @@
 -- creates a table second_table in the database add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table(
-    id INT,
-    name VARCHAR(256),
-    score INT,
-    PRIMARY KEY (id)
-);
+CREATE TABLE 
+	IF NOT EXISTS second_table (
+		id INT,
+		name VARCHAR(256),
+		score INT,
+		PRIMARY KEY (id)
+	);
 
 INSERT INTO second_table(id, name, score)
 VALUES 
-        (1,'John',10),
-        (2,'Alex',3),
-        (3,'Bob',7),
-        (4,'George',8);
+		(1, 'John', 10),
+		(2, 'Alex', 3),
+		(3, 'Bob', 7),
+		(4, 'George', 8);
